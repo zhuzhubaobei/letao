8e3533539e0998f07e74b301a6dcc5e0a8e48b7b
zhuzhubaobei <2284363874@qq.com>
2019-01-05 09:45:34 +0800

    首页完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -12,3 +12,37 @@ $(document).ajaxStop(function () {
     }, 500);
 
 });
+
+
+$(function(){
+    //点击左边侧边栏,显示动画
+    $('.lt_aside .category').click(function(){
+        $(this).next().stop().slideToggle();
+    })
+
+    $('.icon_menu').click(function(){
+        $('.lt_aside').toggleClass('hidemenu');
+        $('.lt_main').toggleClass('hidemenu');
+        $('.lt_topbar').toggleClass('hidemenu');
+    })
+
+    
+    $('.icon_logout').click(function(){
+        $('#logoutModal').modal('show');
+    })
+
+    $('#logoutBtn').click(function(){
+        $.ajax({
+            url: '/employee/checkRootLogin',
+            type: 'get',
+            dataType: 'json',
+            success: function( info ){
+                if ( info.success ){
+                    location.href = 'login.html';
+                }
+            }
+        })
+    })
+
+})
+
